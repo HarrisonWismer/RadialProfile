56adc297f7667dc242a6f96b1aee857793576030
HarrisonWismer <86372850+HarrisonWismer@users.noreply.github.com>
2022-11-04 15:37:26 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+
+setup(
+    name='RadialProfileAnalysis',
+    version='0.1',
+    packages=['napari','pyqt5','aicsimageio','readlif','numpy','matplotlib','pathlib'])
