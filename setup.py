ba71a0c12117b152b38661f8dfa776191196df76
HarrisonWismer <86372850+HarrisonWismer@users.noreply.github.com>
2022-11-04 16:44:12 -0700

    Adding Conda .yml file

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-
-setup(
-    name='RadialProfileAnalysis',
-    version='0.1',
-    packages=['napari','pyqt5','aicsimageio','readlif','numpy','matplotlib','pathlib'])
