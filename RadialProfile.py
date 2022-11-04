5fbdae64de41d95d5fa2d13f91c8fb1586f75d9b
HarrisonWismer <86372850+HarrisonWismer@users.noreply.github.com>
2022-11-04 12:27:21 -0700

    Adding GUI Functionality

--- RadialProfile.py
+++ RadialProfile.py
@@ -1,178 +1,192 @@
 import napari
 import numpy as np
-from aicsimageio import AICSImage
 import matplotlib.pyplot as plt
 import os
 from pathlib import Path
 import tifffile
-import argparse
 
-# Performs Radial Profile Calculation
-# Function Comes From: https://stackoverflow.com/questions/21242011/most-efficient-way-to-calculate-radial-profile
-# Answer provided on StackOverflow by user Bi Rico
-def radial_profile(data, center):
-    y, x = np.indices((data.shape))
-    r = np.sqrt((x - center[0])**2 + (y - center[1])**2).astype(int)
-    
-    tbin = np.bincount(r.ravel(), data.ravel())
-    nr = np.bincount(r.ravel())
-    
-    radialprofile = tbin / nr # MEAN
-    #radialprofile = tbin # SUM
-    
-    return radialprofile
+class RadialProfiler:
+    """
+    Class user to perform radial profile analysis on image data.
+    Inputs:
+        - image -> A AICSImage instance from the aicsimageio package
+        - scenes -> Scene names from the image in which to open Napari Viewers for.
+        - channels -> A List of channel names for each channel in the image
+        - selectedChannel -> The name of the channel from which intensity values will be taken.
+    """
 
-# Checks if a path exists, creating it if it does not
-def checkPath(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
+    def __init__(self, image, scenes, channels, selectedChannel):
+        self.image = image
+        self.scenes = scenes
+        self.channels = channels
+        self.selectedChannel = selectedChannel
 
-
-def main():
-
-    parser = argparse.ArgumentParser(description='Performs a Radial Profile Analysis on User-Defined ROIs')
-    parser.add_argument('-i','--input', help='Provide the Input File Path', required=True)
-    parser.add_argument('-o','--output', help='Provide the Output file Path. If unspecified, write all files to current working directory', required=False)
-    parser.add_argument('-c','--channels', nargs='+', help='Define Channel Names to View in Napari', required=True)
-    parser.add_argument('-s', '--specifiedChannel', help = "Specifiy the Channel Intensity Will Be Measured From", required=True)
-    args = parser.parse_args()
-
-    # Do Error Handling of Command Line Arguments.
-    if args.specifiedChannel not in args.channels:
-        print('Specified Channel "' + args.specifiedChannel + '" is not in List of Channels')
-        exit(1)
-
-    # Read this in with the command line.
-    inputPath = Path(args.input)
-    outputPath = Path(args.output) / Path("RadialProfiles") if args.output != None else Path("./RadialProfiles")
-    checkPath(outputPath)
+    def checkPath(self, path):
+        '''
+        Since the user specifies the output folder, new folders may need to be created.
+        This function takes a PathLib Path object as input and created a directory at the
+        path if it does not already exist.
+        '''
+        if not os.path.exists(path):
+            os.makedirs(path)
 
 
-    # Try to open the specified image.
-    try:
-        img = AICSImage(inputPath)
-    except:
-        print("Invalid Input File: " + str(inputPath))
-        print("Check If Specified Path is Incorrect")
-        exit(1)
+    def radial_profile(self, data, center):
+        '''
+        Performs Radial Profile Calculation
+        Function Comes From: https://stackoverflow.com/questions/21242011/most-efficient-way-to-calculate-radial-profile
+        Answer provided on StackOverflow by user Bi Rico
 
-
-    # The supplied .lif file has multiple scenes, each corresponding to different conditions.
-    for index,scene in enumerate(img.scenes):
-
-        img.set_scene(index)
+        Input:
+            - data: Image data in the form of an array
+            - center: The center-point of the circle
         
-        # Set appropriate channel colors and layer labels
-        labels = args.channels + ["Unspecified" for channel in range(len(args.channels) - 3)]
-        colormaps = ["blue" , "red", "green"] + ["grey" for channel in range(len(args.channels) - 3)]
+        '''
+        y, x = np.indices((data.shape))
+        r = np.sqrt((x - center[0])**2 + (y - center[1])**2).astype(int)
         
-        # In Case the # Of Points != # ROIs
-        dimMatch = False
-        # Stores previous iterations ROI and center information to be added back to re-opened viewer after it
-        # is closed in the case that the # points != # ROIs.
-        roiLayer = None
-        centerLayer = None
+        tbin = np.bincount(r.ravel(), data.ravel())
+        nr = np.bincount(r.ravel())
+        
+        radialprofile = tbin / nr # MEAN
+        #radialprofile = tbin # SUM
+        
+        return radialprofile
 
-        while dimMatch == False:
+    def executeScript(self, outputPath):
+        """
+        This script executes the entire radial profile analysis:
 
-            # Create a viewer of the current image.
-            view = napari.Viewer()
-            view.add_image(img.data,
-                        channel_axis=1, 
-                        name=labels,
-                        colormap=colormaps)
+        Input:
+            - outputPath: An output path specified by the user
+
+        Output:
+            - A folder called RadialProfiles containing:
+                - A single folder for each sample labeled with the sample name each containing:
+                    - A folder for each ROI drawn, named with convention ROI_n, each containing:
+                        - center.csv -> A file containing the center point of the circle used for analysis/
+                        - radial.csv -> A file containing the radial data from the ROI. Can be read using np.loadtxt()
+                        - ROI_n.tiff -> A TIFF file of the ROI with bounding box blacked out.
+                        - RadialPlot.png -> An image of the plotted Radial Profile.
+        """
+
+        outputPath = outputPath / Path("RadialProfiles")
+        self.checkPath(outputPath)
+
+        # The supplied .lif file has multiple scenes, each corresponding to different conditions.
+        for scene in self.scenes:
+
+            index = self.image.scenes.index(scene)
+
+            self.image.set_scene(index)
             
+            # Set appropriate channel colors and layer labels
+            labels = self.channels
+            colormaps = ["blue" , "red", "green"]
+            
+            # In Case the # Of Points != # ROIs
+            dimMatch = False
+            # Stores previous iterations ROI and center information to be added back to re-opened viewer after it
+            # is closed in the case that the # points != # ROIs.
+            roiLayer = None
+            centerLayer = None
 
-            # Create the roiLayer (either with pre-existing or no data)
-            if roiLayer == None:
-                roiLayer = view.add_shapes(name="ROIs")
-            else:
-                if len(roiLayer.data) != 0:
-                    roiLayer = view.add_shapes(roiLayer.data, name="ROIs")
-                else:
+            while dimMatch == False:
+
+                # Create a viewer of the current image.
+                view = napari.Viewer(show=False)
+                view.add_image(self.image.data,
+                            channel_axis=1, 
+                            name=labels,
+                            colormap=colormaps)
+                
+
+                # Create the roiLayer (either with pre-existing or no data)
+                if roiLayer == None:
                     roiLayer = view.add_shapes(name="ROIs")
-            # Create Center Layer (")
-            if centerLayer == None:
-                centerLayer = view.add_points(name="Centers")
-            else:
-                centerLayer = view.add_points(centerLayer.data, name="Centers")
+                else:
+                    if len(roiLayer.data) != 0:
+                        roiLayer = view.add_shapes(roiLayer.data, name="ROIs")
+                    else:
+                        roiLayer = view.add_shapes(name="ROIs")
+                # Create Center Layer (")
+                if centerLayer == None:
+                    centerLayer = view.add_points(name="Centers")
+                else:
+                    centerLayer = view.add_points(centerLayer.data, name="Centers")
 
-            # Halt execution here while user draws ROI's
-            # When the viewer is closed, the rest of the code will run.
-            napari.run()
+                # Halt execution here while user draws ROI's
+                # When the viewer is closed, the rest of the code will run.
+                view.show(block=True)
 
-            # If the # Centers == # ROIs, clear pevious data (moves on to next slide)
-            # Else the viewer will re-open with previous data.
-            if len(centerLayer.data) == len(roiLayer.data):
-                dimMatch = True
-                roiLayer = False
-                centerLayer = False
+                # If the # Centers == # ROIs, clear pevious data (moves on to next slide)
+                # Else the viewer will re-open with previous data.
+                if len(centerLayer.data) == len(roiLayer.data):
+                    dimMatch = True
+                    roiLayer = False
+                    centerLayer = False
 
 
-        # This grabs the (X x Y) image size into variables x and y
-        s1, s2, x, y = view.layers[0].data.shape
+            # This grabs the (X x Y) image size into variables x and y
+            s1, s2, x, y = view.layers[0].data.shape
 
-        # Creates a mask for every ROI drawn which is then used to "crop" the image.
-        masks = view.layers["ROIs"].to_masks(mask_shape=(x,y))
+            # Creates a mask for every ROI drawn which is then used to "crop" the image.
+            masks = view.layers["ROIs"].to_masks(mask_shape=(x,y))
 
-        # User can draw ROI's on whichever Z-Slice they want. Save the current Z-Slice.
-        currZ = view.dims.current_step[1]
-        
-        # Create the folder within the current working directory to save all of the ROI information.
-        scenePath = outputPath / scene
-        checkPath(scenePath)
+            # User can draw ROI's on whichever Z-Slice they want. Save the current Z-Slice.
+            currZ = view.dims.current_step[1]
+
+            # Create the folder within the current working directory to save all of the ROI information.
+            scenePath = outputPath / scene
+            self.checkPath(scenePath)
             
-        
-        for index in range(len(view.layers["Centers"].data)):
+            for index in range(len(view.layers["Centers"].data)):
 
-            # Get current info
-            currCenter = view.layers["Centers"].data[index]
-            currRoi = view.layers["ROIs"].data[index]
-            currMask = masks[index]
+                # Get current info
+                currCenter = view.layers["Centers"].data[index]
+                currRoi = view.layers["ROIs"].data[index]
+                currMask = masks[index]
 
-            # Create Mask for Cropping and add it as a new layer
-            maskArray = np.where(currMask==1, view.layers[args.specifiedChannel].data , currMask.astype(int))
-            view.add_image(maskArray, name = "ROI_" + str(index))
+                # Create Mask for Cropping and add it as a new layer
+                maskArray = np.where(currMask==1, view.layers[self.selectedChannel].data , currMask.astype(int))
+                view.add_image(maskArray, name = "ROI_" + str(index))
 
-            # Get Min and Max x and y coordinates to create a new image from the cropped image
-            xmin, ymin = np.min(currRoi, axis=0).astype(int) 
-            xmax, ymax = np.max(currRoi, axis=0).astype(int)
+                # Get Min and Max x and y coordinates to create a new image from the cropped image
+                xmin, ymin = np.min(currRoi, axis=0).astype(int) 
+                xmax, ymax = np.max(currRoi, axis=0).astype(int)
 
-            if xmin < 0: xmin = 0
-            if xmax > x: xmax = x
-            if ymin < 0: ymin = 0
-            if ymax > y: ymax = y 
+                if xmin < 0: xmin = 0
+                if xmax > x: xmax = x
+                if ymin < 0: ymin = 0
+                if ymax > y: ymax = y 
 
-            # Create numpy array of cropped image.
-            cropped = view.layers["ROI_" + str(index)].data[0][currZ][xmin:xmax,ymin:ymax]
+                # Create numpy array of cropped image.
+                cropped = view.layers["ROI_" + str(index)].data[0][currZ][xmin:xmax,ymin:ymax]
 
-            # Save cropped ROI image
-            roiPath = scenePath / Path("ROI_" + str(index))
-            checkPath(roiPath)
-            imgPath = roiPath / Path("ROI_" + str(index) + ".tiff")
-            tifffile.imwrite(imgPath  , cropped)
+                # Save cropped ROI image
+                roiPath = scenePath / Path("ROI_" + str(index))
+                self.checkPath(roiPath)
+                imgPath = roiPath / Path("ROI_" + str(index) + ".tiff")
+                tifffile.imwrite(imgPath  , cropped)
 
-            oldX, oldY = currCenter[0], currCenter[1]
-            newX, newY = int(oldX - xmin), int(oldY-ymin)
+                oldX, oldY = currCenter[0], currCenter[1]
+                newX, newY = int(oldX - xmin), int(oldY-ymin)
 
-            # Save the center point information into a CSV.
-            csvPath = roiPath / Path("center.csv")
-            with open(csvPath, 'w') as f:
-                f.write(str(newX) + "," + str(newY))
+                # Save the center point information into a CSV.
+                csvPath = roiPath / Path("center.csv")
+                with open(csvPath, 'w') as f:
+                    f.write(str(newX) + "," + str(newY))
 
-            # Calculate the radial profile
-            rad = radial_profile(cropped, (newY,newX))
+                # Calculate the radial profile
+                rad = self.radial_profile(cropped, (newY,newX))
 
-            # Save the Radial Profile Data into a csv
-            radPath = roiPath / Path("radial.csv")
-            with open(radPath, "w") as f:
-                for index,intensity in enumerate(rad):
-                    f.write(str(intensity))
-                    f.write("\n")
+                # Save the Radial Profile Data into a csv
+                radPath = roiPath / Path("radial.csv")
+                with open(radPath, "w") as f:
+                    for index,intensity in enumerate(rad):
+                        f.write(str(intensity))
+                        f.write("\n")
 
-            plt.plot(rad)
-            plt.savefig(roiPath / Path("RadialPlot.png"))
-            plt.close()
-
-if __name__ == "__main__":
-    main()
+                plt.plot(rad)
+                plt.savefig(roiPath / Path("RadialPlot.png"))
+                plt.close()
---
+++ radialProfileWindow.py
@@ -0,0 +1,130 @@
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QFileDialog, QApplication, QListView
+from PyQt5.uic import loadUi
+
+from aicsimageio import AICSImage
+from pathlib import Path
+
+import RadialProfile as rp
+
+class MainWindow(QMainWindow):
+    """
+    Builds off of radialUI.ui to create a MainWindow child.
+
+    Contains all necessary events to interface with the RadialProfile module to run
+    radial profile analysis as outlined in the module.
+    """
+
+    def __init__(self):
+
+        super(MainWindow,self).__init__()
+        loadUi("radialUI.ui", self)
+
+        # Create RadialProfile Instance Once Able to
+        self.rp = None
+        # Attributes needed to instantiate RadialProfile instance.
+        self.image = None
+        self.scenes = None
+        self.channels = None
+        self.selectedChannel = None
+
+        # Click Events for UI
+        self.loadBrowse.clicked.connect(self.browseInputFiles)
+        self.outputBrowse.clicked.connect(self.browseOutputFiles)
+        self.selectAllSamples.clicked.connect(self.selectAllScenes)
+        self.clearAllSamples.clicked.connect(self.clearAllScenes)
+        self.sampleList.itemSelectionChanged.connect(self.loadUpScenes)
+        self.channelList.itemSelectionChanged.connect(self.channelSelection)
+        self.runButton.clicked.connect(self.createRadialProfile)
+
+
+    def browseInputFiles(self):
+        """
+        This reads in an image file specified by the user (currently only tested on .LIF files)
+        """
+        # Allow user to browse through computer files to select input file.
+        file = QFileDialog.getOpenFileName(self, "Select Input File")
+        self.inputLine.setText(file[0])
+
+        try:
+            path = Path(self.inputLine.text())
+            self.image = AICSImage(path)
+            self.sampleList.addItems(list(self.image.scenes))
+
+            # Assumes each sample has the same number ofchannels
+            nChannels = self.image.data.shape[1]
+            self.channels = ["Channel_" + str(num+1) for num in range(nChannels)]
+            self.channelList.addItems(self.channels)
+
+        except:
+            self.inputLine.setText("Not a Valid .LIF file")\
+
+    def browseOutputFiles(self):
+        """
+        Allows user to select an ouput directory.
+        """
+        file = QFileDialog.getExistingDirectory(self, "Select Output Directory")
+        self.outputLine.setText(file)
+    
+    def selectAllScenes(self):
+        """
+        Select all scenes/samples from the QListWidget.
+        """
+        self.sampleList.selectAll()
+
+    def clearAllScenes(self):
+        """
+        Deselect/clear all scenes/samples from QListWidget.
+        """
+        self.sampleList.clearSelection()
+
+    def loadUpScenes(self):
+        """
+        Load the scenes upon user selecting them into the list of scenes to be run.
+        """
+        self.scenes = [sample.text() for sample in self.sampleList.selectedItems()]
+
+    def channelSelection(self):
+        """
+        Get the single channel to be used to measure intensity upon selection in the QListWidget.
+        """
+        self.selectedChannel = [channel.text() for channel in self.channelList.selectedItems()][0]
+
+    def createRadialProfile(self):
+        """
+        This should be the final step in the GUI procedure. If any of the attributes are None, the RadialProfiler
+        object will not be able to be instantiated. This checks if any of these attributes are none and displays a
+        message if one of the steps has been missed.
+        """
+
+        attributes = [self.image, self.scenes,self.channels,self.selectedChannel]
+        messages = ["Image Not Loaded In", "Scenes Not Selected", "Channels Not Loaded", "Channels Not Selected"]
+
+        try:
+            print(self.image, self.scenes)
+            self.rp = rp.RadialProfiler(self.image, self.scenes, self.channels, self.selectedChannel)
+            self.rp.executeScript(Path(self.outputLine.text())) # Run the Analysis
+
+        except:
+            for attr,mes in zip(attributes,messages):
+                if attr == None:
+                    print(mes)
+
+
+def main():
+    app = QApplication(sys.argv)
+    mainWindow = MainWindow()
+    widget=QtWidgets.QStackedWidget()
+    widget.addWidget(mainWindow)
+    widget.setFixedWidth(530)
+    widget.setFixedHeight(450)
+    widget.show()
+    sys.exit(app.exec())
+if __name__=="__main__":
+    main()
+
+
+
+
+
